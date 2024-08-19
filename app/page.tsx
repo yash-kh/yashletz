9b7b2a938ef9f0fbf8e05acc4b73428d8d146cb2
yash-kh <yashkhatri2198@gmail.com>
2024-08-19 21:21:07 +0530

    basic wallet key logic added

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
-import Image from "next/image";
+"use client";
+
+import { Keypair } from "@solana/web3.js";
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
+import { derivePath } from "ed25519-hd-key";
+import { Wallet } from "ethers";
+import { HDNodeWallet } from "ethers";
+import { useState } from "react";
+import nacl from "tweetnacl";
 
 export default function Home() {
+  const [mnemonic, setMnemonic] = useState("");
+  const [mnemonicSecured, setMnemonicSecured] = useState(false);
+  const [ethereumWallets, setEthereumWallets] = useState<string[]>([]);
+  const [solanaWallets, setSolanaWallets] = useState<string[]>([]);
+  const [walletsCreated, setWalletsCreated] = useState(false);
+
+  function generateStarterWallet() {
+    // solana
+    generateEthereumWallets();
+
+    // ethereum
+    generateSolanaWallets();
+    setWalletsCreated(true);
+  }
+
+  function generateEthereumWallets() {
+    const derivationPath = `m/44'/60'/${ethereumWallets.length}'/0'`;
+    const hdNode = HDNodeWallet.fromSeed(mnemonicToSeedSync(mnemonic));
+    const child = hdNode.derivePath(derivationPath);
+    const privateKey = child.privateKey;
+    const wallet = new Wallet(privateKey);
+    setEthereumWallets([...ethereumWallets, wallet.address]);
+  }
+
+  function generateSolanaWallets() {
+    const seed = mnemonicToSeedSync(mnemonic);
+    const path = `m/44'/501'/${solanaWallets.length}'/0'`;
+    const derivedSeed = derivePath(path, seed.toString("hex")).key;
+    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+    const keypair = Keypair.fromSecretKey(secret);
+    setSolanaWallets([...solanaWallets, keypair.publicKey.toString()]);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      {!walletsCreated && mnemonic && (
+        <>
+          <main className="flex min-h-screen flex-col gap-10 items-center p-24">
+            <h1 className="text-3xl font-bold">Welcome to Yashletz!</h1>
+            <p className="text-2xl">
+              Easily manage and use your cryptocurrency.
+            </p>
+            <p>{mnemonic}</p>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <label htmlFor="mnemonic-secured">
+              <input
+                type="checkbox"
+                checked={mnemonicSecured}
+                onChange={() => setMnemonicSecured(!mnemonicSecured)}
+              />{" "}
+              Secured
+            </label>
+            <button onClick={generateStarterWallet} disabled={!mnemonicSecured}>
+              Generate Wallet
+            </button>
+          </main>
+        </>
+      )}
+      {!walletsCreated && !mnemonic && (
+        <>
+          <main className="flex min-h-screen flex-col gap-10 items-center p-24">
+            <h1 className="text-3xl font-bold">Welcome to Yashletz!</h1>
+            <p className="text-2xl">
+              Easily manage and use your cryptocurrency.
+            </p>
+            <button onClick={() => setMnemonic(generateMnemonic())}>
+              Generate Security phrases
+            </button>
+          </main>
+        </>
+      )}
+      {walletsCreated && (
+        <>
+          <main className="flex min-h-screen flex-col gap-10 items-center p-24">
+            <h1 className="text-3xl font-bold">Welcome to Yashletz!</h1>
+            <p className="text-2xl">
+              Easily manage and use your cryptocurrency.
+            </p>
+            <p>
+              Ethereum <button onClick={generateEthereumWallets}>+</button>
+            </p>
+            <ul>
+              {ethereumWallets.map((wallet) => (
+                <li key={wallet}>{wallet}</li>
+              ))}
+            </ul>
+            <p>
+              Solana <button onClick={generateSolanaWallets}>+</button>
+            </p>
+            <ul>
+              {solanaWallets.map((wallet) => (
+                <li key={wallet}>{wallet}</li>
+              ))}
+            </ul>
+          </main>
+        </>
+      )}
+    </>
   );
 }
